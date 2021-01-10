9ac6c3b7ec9066d5fe510293c1aeadaaca9a043d
Clément POIRET <poiret.clement@outlook.fr>
2021-01-10 15:28:46 +0100

    Added wrapper around corr functions * Pearson, * Spearman, * Kendall, * Biweight midcorrelation, * Percentage bend correlation, * Shepherd's pi correlation.

--- src/correlation.jl
+++ src/correlation.jl
@@ -1,3 +1,7 @@
+include("bayesian.jl")
+include("effsize.jl")
+
+using DataFrames
 using Distributions
 using Statistics
 using StatsBase
@@ -268,4 +272,271 @@ function bicor(x::Array{<:Number},
     pval = 2 * ccdf(TDist(nx - 2), abs(tval))
 
     return r, pval
+end
+
+
+"""
+    corr(x, y[, tail, method])
+
+(Robust) correlation between two variables.
+
+Arguments
+----------
+- `x, y::Array{<:Number}`: First and second set of observations. ``x`` and ``y`` must be independent.
+- `tail::String`: Specify whether to return `one-sided` or `two-sided` p-value. Note that the former are simply half the latter.
+- `method::String`: Correlation type:
+    * `pearson`: Pearson \$r\$ product-moment correlation
+    * `spearman`: Spearman's \$\rho\$ rank-order correlation
+    * `kendall`: Kendall's \$\tau\$ correlation (for ordinal data)
+    * `bicor`: Biweight midcorrelation (robust)
+    * `percbend`: Percentage bend correlation (robust)
+    * `shepherd`: Shepherd's pi correlation (robust)
+
+Returns
+-------
+- `stats::DataFrame`:
+    * `n`: Sample size (after removal of missing values)
+    * `outliers`: number of outliers, only if a robust method was used
+    * `r`: Correlation coefficient
+    * `CI95`: 95% parametric confidence intervals around `r`
+    * `r2`: R-squared (`= r^2`)
+    * `adj_r2`: Adjusted R-squared
+    * `p-val`: tail of the test
+    * `BF10`: Bayes Factor of the alternative hypothesis (only for Pearson correlation)
+    * `power`: achieved power of the test (= 1 - type II error).
+
+See also
+--------
+[`pairwise_corr`](@ref): Pairwise correlation between columns of a pandas DataFrame
+[`partial_corr`](@ref): Partial correlation
+[`rm_corr`](@ref): Repeated measures correlation
+
+Notes
+-----
+The `Pearson correlation coefficient
+<https://en.wikipedia.org/wiki/Pearson_correlation_coefficient>`_
+measures the linear relationship between two datasets. Strictly speaking,
+Pearson's correlation requires that each dataset be normally distributed.
+Correlations of -1 or +1 imply a perfect negative and positive linear
+relationship, respectively, with 0 indicating the absence of association.
+
+\$\$ r_{xy} = \\frac{\\sum_i(x_i - \\bar{x})(y_i - \\bar{y})} {\\sqrt{\\sum_i(x_i - \\bar{x})^2} \\sqrt{\\sum_i(y_i - \\bar{y})^2}} = \\frac{\\text{cov}(x, y)}{\\sigma_x \\sigma_y} \$\$
+
+where \$cov\$ is the sample covariance and \$sigma\$
+is the sample standard deviation.
+
+If `method='pearson'`, The Bayes Factor is calculated using the
+`Pingouin.bayesfactor_pearson` function.
+
+The `Spearman correlation coefficient
+<https://en.wikipedia.org/wiki/Spearman%27s_rank_correlation_coefficient>`_
+is a non-parametric measure of the monotonicity of the relationship between
+two datasets. Unlike the Pearson correlation, the Spearman correlation does
+not assume that both datasets are normally distributed. Correlations of -1
+or +1 imply an exact negative and positive monotonic relationship,
+respectively. Mathematically, the Spearman correlation coefficient is
+defined as the Pearson correlation coefficient between the
+`rank variables <https://en.wikipedia.org/wiki/Ranking>`.
+
+The `Kendall correlation coefficient
+<https://en.wikipedia.org/wiki/Kendall_rank_correlation_coefficient>`_
+is a measure of the correspondence between two rankings. Values also range
+from -1 (perfect disagreement) to 1 (perfect agreement), with 0 indicating
+the absence of association. Consistent with
+:py:func:`scipy.stats.kendalltau`, Pingouin returns the Tau-b coefficient,
+which adjusts for ties:
+
+\$\$ \\tau_B = \\frac{(P - Q)}{\\sqrt{(P + Q + T) (P + Q + U)}} \$\$
+
+where \$P\$ is the number of concordant pairs, \$Q\$ the number of
+discordand pairs, \$T\$ the number of ties in x, and \$U\$
+the number of ties in y.
+
+The `biweight midcorrelation
+<https://en.wikipedia.org/wiki/Biweight_midcorrelation>` and
+percentage bend correlation [1]_ are both robust methods that
+protects against *univariate* outliers by down-weighting observations that
+deviate too much from the median.
+
+The Shepherd pi [2] correlation and skipped [3], [4] correlation are
+both robust methods that returns the Spearman correlation coefficient after
+removing *bivariate* outliers. Briefly, the Shepherd pi uses a
+bootstrapping of the Mahalanobis distance to identify outliers, while the
+skipped correlation is based on the minimum covariance determinant
+(which requires scikit-learn). Note that these two methods are
+significantly slower than the previous ones.
+
+**important: Please note that rows with missing values (NaN) will be automatically removed in a later version. For now, please remove them before calling the function.**
+
+References
+----------
+[1] Wilcox, R.R., 1994. The percentage bend correlation coefficient.
+Psychometrika 59, 601–616. https://doi.org/10.1007/BF02294395
+
+[2] Schwarzkopf, D.S., De Haas, B., Rees, G., 2012. Better ways to
+improve standards in brain-behavior correlation analysis. Front.
+Hum. Neurosci. 6, 200. https://doi.org/10.3389/fnhum.2012.00200
+
+[3] Rousselet, G.A., Pernet, C.R., 2012. Improving standards in
+brain-behavior correlation analyses. Front. Hum. Neurosci. 6, 119.
+https://doi.org/10.3389/fnhum.2012.00119
+
+[4] Pernet, C.R., Wilcox, R., Rousselet, G.A., 2012. Robust correlation
+analyses: false positive and power validation using a new open
+source matlab toolbox. Front. Psychol. 3, 606.
+https://doi.org/10.3389/fpsyg.2012.00606
+
+Examples
+--------
+1. Pearson correlation
+
+```julia-repl
+julia> x = [1,4,2,5,8,9,6,5,4]
+julia> y = [4,5,8,7,5,4,1,2,5]
+julia> # Compute Pearson correlation
+julia> Pingouin.corr(x, y)
+┌ Warning: P-Value not implemented yet in HypothesisTests.jl
+└ @ Main REPL[59]:11
+1×9 DataFrame
+ Row │ n      outliers  r          CI95                  r2        adj_r2     p_val    BF10      power   
+     │ Int64  Float64   Float64    Array…                Float64   Float64    Float64  Float64   Float64 
+─────┼───────────────────────────────────────────────────────────────────────────────────────────────────
+   1 │     9      NaN   -0.299022  [-0.803566, 0.45557]  0.089414  -0.214115     NaN   0.533201     NaN
+```
+
+2. Pearson correlation with two outliers
+
+```julia-repl
+julia> x[3], y[5] = 12, -8
+julia> Pingouin.corr(x, y)
+┌ Warning: P-Value not implemented yet in HypothesisTests.jl
+└ @ Main REPL[59]:11
+1×9 DataFrame
+ Row │ n      outliers  r           CI95                   r2          adj_r2    p_val    BF10      power   
+     │ Int64  Float64   Float64     Array…                 Float64     Float64   Float64  Float64   Float64 
+─────┼──────────────────────────────────────────────────────────────────────────────────────────────────────
+   1 │     9      NaN   -0.0410185  [-0.686441, 0.640553]  0.00168252  -0.33109     NaN   0.408345     NaN
+```
+
+3. Spearman correlation (robust to outliers)
+
+```julia-repl
+julia> Pingouin.corr(x, y, method="spearman")
+┌ Warning: P-Value not implemented yet in HypothesisTests.jl
+└ @ Main REPL[68]:15
+1×9 DataFrame
+ Row │ n      outliers  r           CI95                   r2          adj_r2     p_val    BF10     power   
+     │ Int64  Float64   Float64     Array…                 Float64     Float64    Float64  Float64  Float64 
+─────┼──────────────────────────────────────────────────────────────────────────────────────────────────────
+   1 │     9      NaN   -0.0423729  [-0.687157, 0.639752]  0.00179546  -0.330939     NaN      NaN      NaN
+```
+
+4. Biweight midcorrelation (robust)
+
+```julia-repl
+julia> Pingouin.corr(x, y, method="bicor")
+1×9 DataFrame
+ Row │ n      outliers  r         CI95                  r2         adj_r2     p_val     BF10     power   
+     │ Int64  Float64   Float64   Array…                Float64    Float64    Float64   Float64  Float64 
+─────┼───────────────────────────────────────────────────────────────────────────────────────────────────
+   1 │     9      NaN   -0.28545  [-0.798245, 0.46725]  0.0814815  -0.224691  0.456539     NaN      NaN
+```
+
+5. Percentage bend correlation (robust)
+
+```julia-repl
+julia> Pingouin.corr(x, y, method="percbend")
+1×9 DataFrame
+ Row │ n      outliers  r           CI95                   r2          adj_r2     p_val     BF10     power   
+     │ Int64  Float64   Float64     Array…                 Float64     Float64    Float64   Float64  Float64 
+─────┼───────────────────────────────────────────────────────────────────────────────────────────────────────
+   1 │     9      NaN   0.00520452  [-0.661203, 0.667021]  2.70871e-5  -0.333297  0.989398     NaN      NaN
+```
+
+6. Shepherd's pi correlation (robust)
+
+```julia-repl
+julia> Pingouin.corr(x, y, method="shepherd")
+┌ Warning: P-Value not implemented yet in HypothesisTests.jl
+└ @ Main REPL[68]:25
+1×9 DataFrame
+ Row │ n      outliers  r           CI95                   r2          adj_r2     p_val    BF10     power   
+     │ Int64  Int64     Float64     Array…                 Float64     Float64    Float64  Float64  Float64 
+─────┼──────────────────────────────────────────────────────────────────────────────────────────────────────
+   1 │     9         0  -0.0423729  [-0.687157, 0.639752]  0.00179546  -0.330939     NaN      NaN      NaN
+```
+
+7. One-tailed Pearson correlation
+
+```julia-repl
+julia> Pingouin.corr(x, y, tail="one-sided", method="pearson")
+┌ Warning: P-Value not implemented yet in HypothesisTests.jl
+└ @ Main REPL[68]:11
+1×9 DataFrame
+ Row │ n      outliers  r           CI95                   r2          adj_r2    p_val    BF10      power   
+     │ Int64  Float64   Float64     Array…                 Float64     Float64   Float64  Float64   Float64 
+─────┼──────────────────────────────────────────────────────────────────────────────────────────────────────
+   1 │     9      NaN   -0.0410185  [-0.686441, 0.640553]  0.00168252  -0.33109     NaN   0.439293     NaN
+```
+"""
+function corr(x::Array{<:Number},
+              y::Array{<:Number};
+              tail::String="two-sided",
+              method::String="pearson")
+    # todo: Remove rows with missing values
+    # todo: update when p-values available in hypothesistests.jl
+    nx = size(x)[1]
+    r = pval = bf10 = outliers = pr = NaN
+    # Compute correlation coefficient
+    if method == "pearson"
+        @warn "P-Value not implemented yet in HypothesisTests.jl"
+        r = cor(x, y)
+        bf10 = bayesfactor_pearson(r, nx, tail=tail)
+    elseif method == "spearman"
+        @warn "P-Value not implemented yet in HypothesisTests.jl"
+        r = corspearman(x, y)
+    elseif method == "kendall"
+        @warn "P-Value not implemented yet in HypothesisTests.jl"
+        r = corkendall(x, y)
+    elseif method == "bicor"
+        r, pval = bicor(x, y)
+    elseif method == "percbend"
+        r, pval = percbend(x, y)
+    elseif method == "shepherd"
+        @warn "P-Value not implemented yet in HypothesisTests.jl"
+        r, outliers = shepherd(x, y)
+        outliers = sum(outliers)
+    else
+        throw(DomainError(method, "Method not recognized."))
+    end
+
+    if r == NaN
+        return DataFrame(n=nx,
+                         r=NaN,
+                         CI95=NaN,
+                         r2=NaN,
+                         adj_r2=NaN,
+                         p_val=NaN,
+                         BF10=NaN,
+                         power=NaN)
+    end
+
+    # Compute r2 and adj_r2
+    r2 = r^2
+    adj_r2 = 1 - (((1 - r2) * (nx - 1)) / (nx - 3))
+
+    # Compute the parametric 95% confidence interval and power
+    ci = compute_esci(stat=r, nx=nx, ny=nx, eftype="r", decimals=6)
+    # todo: implement power_corr
+    # pr = power_corr(r=r, n=nx, power=None, alpha=0.05, tail=tail)
+
+    return DataFrame(n=nx,
+                     outliers=outliers,
+                     r=r,
+                     CI95=[ci],
+                     r2=r2,
+                     adj_r2=adj_r2,
+                     p_val=pval,
+                     BF10=bf10,
+                     power=pr)
 end
