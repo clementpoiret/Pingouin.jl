91b40988da779faced8fd7feb23cd610f2542b46
Clément POIRET <poiret.clement@outlook.fr>
2021-12-15 09:57:11 +0100

    Added p-value from permutation test

---
+++ src/utils.jl
@@ -0,0 +1,36 @@
+using DataFrame
+
+"""
+    _perm_pval(bootstat, estimate[, alternative])
+
+Compute p-values from a permutation test.
+
+Arguments
+---------
+- `bootstat::Array{<:Number}`: Permation distribution.
+- `estimate::Number`: Point estimate.
+- `alternative::String`: Tail for p-value. One of "two-sided" (default), "less", "greater".
+
+Returns
+-------
+- `pval::Float64`: P-value.
+"""
+function _perm_pval(bootstat::Array{<:Number},
+                    estimate::Number,
+                    alternative::String)::Float64
+
+    @assert tail in ["two-sided", "less", "greater"] "Tail must be \"two-sided\", \"less\" or \"greater\"."
+
+    n_boot = length(bootstat)
+    @assert n_boot > 0 "Bootstrap distribution must have at least one element."
+
+    if (tail == "greater") {
+        pval = sum(bootstat >= estimate) / n_boot
+    } else if (tail == "less") {
+        pval = sum(bootstat <= estimate) / n_boot
+    } else {
+        pval = sum(abs(bootstat) >= abs(estimate)) / n_boot
+    }
+
+    return pval
+end
