fb6d91361369ee8f28019f73f21c5e3e2aaa2555
Clément POIRET <poiret.clement@outlook.fr>
2021-12-15 11:16:51 +0100

    Added `_remove_na_single`

--- src/utils.jl
+++ src/utils.jl
@@ -34,3 +34,33 @@ function _perm_pval(bootstat::Vector{<:Number},
 
     return pval
 end
+
+
+# Missing values
+"""
+    _remove_na_single(x[, axis])
+
+Remove NaN in a single array.
+
+Arguments
+---------
+- `x::Array{<:Number}`. Array to filter.
+- `axis::String`. `rows` or `cols` to filter when `x` is a 2D array.
+
+Returns
+-------
+- `x::Array{<:Number}`. Filtered array.
+"""
+function _remove_na_single(x::Vector{<:Number})::Vector{<:Number}
+    return @. x[!isnan(x)]
+end
+function _remove_na_single(x::Array{<:Number,2}; axis::String = "rows")::Array{<:Number,2}
+    dim = axis == "rows" ? 2 : 1
+    nan_idx = any(isnan.(x), dims = dim)
+
+    if axis == "rows"
+        return x[vec(@. !nan_idx), :]
+    else
+        return x[:, vec(@. !nan_idx)]
+    end
+end
