0e55cad4c21744be8a3ca15da7c4320b1ed55225
Clément POIRET <poiret.clement@outlook.fr>
2021-12-15 16:59:38 +0100

    `remove_na` for 2D arrays

--- src/utils.jl
+++ src/utils.jl
@@ -111,66 +111,116 @@ julia> Pingouin.remove_na(x, axis="cols")
 3. With two paired 1D arrays
 
 ```julia-repl
-julia> y = [2.3, NaN, 5.2, 4.6]
-julia> Pingouin.remova_na(x, y, paired=true)
-#todo
+julia> x = [6.4, 3.2, 4.5, NaN];
+julia> y = [2.3, NaN, 5.2, 4.6];
+julia> Pingouin.remove_na(x, y, paired=true)
+([6.4, 4.5], [2.3, 5.2])
 ```
 
 4. With two independent 2D arrays
 
 ```julia-repl
-julia> x = [4 2; 4 NaN; 7 6]
-julia> y = [6 NaN; 3 2; 2 2]
-julia> x_no_nan, y_no_nan = Pingouin.remove_na(x, y, paired=false)
-#todo
+julia> x = [4 2; 4 NaN; 7 6];
+julia> y = [6 NaN; 3 2; 2 2];
+julia> x_no_nan, y_no_nan = Pingouin.remove_na(x, y, paired=false);
+julia> x_no_nan
+2×2 Matrix{Float64}:
+ 4.0  2.0
+ 7.0  6.0
+julia> y_no_nan
+2×2 Matrix{Float64}:
+ 3.0  2.0
+ 2.0  2.0
+```
+
+5. With two paired 2D arrays
+
+```julia-repl
+julia> x_no_nan, y_no_nan = Pingouin.remove_na(x, y, paired=true);
+julia> x_no_nan
+1×2 Matrix{Float64}:
+ 7.0  6.0
+julia> y_no_nan
+1×2 Matrix{Float64}:
+ 2.0  2.0
+julia> x_no_nan, y_no_nan = Pingouin.remove_na(x, y, paired=true, axis="cols");
+julia> x_no_nan
+3×1 Matrix{Float64}:
+ 4.0
+ 4.0
+ 7.0
+julia> y_no_nan
+3×1 Matrix{Float64}:
+ 6.0
+ 3.0
+ 2.0
 ```
 """
+# 1D
 function remove_na(x::Vector{<:Number})::Vector{<:Number}
     @assert length(x) > 0 "Input array must have at least one element."
 
     return _remove_na_single(x)
-end
-function remove_na(x::Array{<:Number,2}; axis::String = "rows")::Array{<:Number,2}
-    @assert length(x) > 0 "Input array must have at least one element."
-
-    return _remove_na_single(x, axis = axis)
 end
 function remove_na(x::Vector{<:Number},
     y::Union{<:Number,AbstractString})::Tuple{Vector{<:Number},Union{<:Number,AbstractString}}
 
     @assert length(x) > 0 "Input array must have at least one element."
 
-    return _remove_na_single(x, axis = axis), y
+    return _remove_na_single(x), y
 end
 function remove_na(x::Vector{<:Number},
     y::Vector{<:Number};
-    paired::Bool)::Tuple{Vector{<:Number}}
+    paired::Bool)::Tuple{Vector{<:Number},Vector{<:Number}}
 
     @assert length(x) > 0 "Input array must have at least one element."
-    @assert size(x) == size(y) "x and y arrays must have the same shape."
-    error("unimplemented")
 
-    return x, y
+    if length(y) == 1
+        return _remove_na_single(x), y
+    elseif (size(x) != size(y)) | !paired
+        return _remove_na_single(x), _remove_na_single(y)
+    end
+
+    # Let's assume it's paired with equal sizes
+    @assert size(x) == size(y) "x and y arrays must have the same shape."
+
+    x_mask = @. !isnan(x)
+    y_mask = @. !isnan(y)
+    both = x_mask .& y_mask
+
+    return x[vec(both)], y[vec(both)]
 end
-function remove_na(x::Vector{<:Number},
-    y::Vector{<:Number};
-    paired::Bool)::Tuple{Vector{<:Number}}
-
+# 2D
+function remove_na(x::Array{<:Number,2}; axis::String = "rows")::Array{<:Number,2}
     @assert length(x) > 0 "Input array must have at least one element."
-    @assert size(x) == size(y) "x and y arrays must have the same shape."
-    error("unimplemented")
 
-    return x, y
+    return _remove_na_single(x, axis = axis)
 end
 function remove_na(x::Array{<:Number,2},
     y::Array{<:Number,2};
     paired::Bool,
-    axis::String = "rows")::Tuple{Array{<:Number,2}}
+    axis::String = "rows")::Tuple{Array{<:Number,2},Array{<:Number,2}}
 
     @assert length(x) > 0 "Input array must have at least one element."
-    @assert size(x) == size(y) "x and y arrays must have the same shape."
     @assert axis in ["rows", "cols"] "Axis must be \"rows\" or \"cols\"."
-    error("unimplemented")
 
-    return x, y
+    if length(y) == 1
+        return _remove_na_single(x, axis = axis), y
+    elseif (size(x) != size(y)) | !paired
+        return _remove_na_single(x, axis = axis), _remove_na_single(y, axis = axis)
+    end
+
+    # Let's assume it's paired with equal sizes
+    @assert size(x) == size(y) "x and y arrays must have the same shape."
+
+    dim = axis == "rows" ? 2 : 1
+    x_mask = any(isnan.(x), dims = dim)
+    y_mask = any(isnan.(y), dims = dim)
+    both = @. !x_mask & !y_mask
+
+    if axis == "rows"
+        return x[vec(both), :], y[vec(both), :]
+    else
+        return x[:, vec(both)], y[:, vec(both)]
+    end
 end
